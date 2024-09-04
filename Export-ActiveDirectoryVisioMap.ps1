301b7a23ed05abd6e08dedbc7e440ca0e5706c6e
Kyle Schuler <kyschuler@gmail.com>
2024-09-04 16:14:41 -0400

    Update Export-ActiveDirectoryVisioMap.ps1
    Added options for user to include or exclude GPOs, and choose the direction of the layout, and cleaned up some output

--- Export-ActiveDirectoryVisioMap.ps1
+++ Export-ActiveDirectoryVisioMap.ps1
@@ -20,6 +20,7 @@
 #
 #############################################################################
 
+Clear-Host
 Write-Output "Starting up..."
 
 #Import the modules
@@ -47,6 +48,42 @@ Catch {
         exit
     }
 }
+
+################################################################
+# Adjust the following variables to suit your environment
+# Set up options
+$IncludeGPOs = $true
+$LayoutDirection = "TopToBottom"
+################################################################
+
+
+
+
+
+# Get user input
+do{
+    if($IncludeGPOs){ $IncludeGPOsInput = "Y" }
+    else { $IncludeGPOsInput = "N" }
+    Write-Output ""
+    Write-Output "Would you like to include GPOs in the map? Y/N"
+    $IncludeGPOsInput = Read-Host "Type Y or N and press Enter, or press Enter to accept the default of $IncludeGPOsInput"
+    if($IncludeGPOsInput.ToUpper() -eq "Y") { $IncludeGPOs = $true; break }
+    elseif($IncludeGPOsInput.ToUpper() -eq "N") { $IncludeGPOs = $false; break }
+    else { Write-Output "Invalid input. Please try again." }
+} while($true)
+
+do{
+    if($LayoutDirection -eq "TopToBottom") { $LayoutDirectionInput = "1" }
+    else { $LayoutDirectionInput = "2" }
+    Write-Output ""
+    Write-Output "Choose the direction of the layout:"
+    Write-Output "1. Top to Bottom"
+    Write-Output "2. Left to Right"
+    $LayoutDirectionInput = Read-Host "Type 1 or 2 and press Enter, or press Enter to accept the default of $LayoutDirectionInput"
+    if($LayoutDirectionInput -eq "1") { $LayoutDirection = "TopToBottom"; break }
+    elseif($LayoutDirectionInput -eq "2") { $LayoutDirection = "LeftToRight"; break }
+    else { Write-Output "Invalid input. Please try again." }
+} while($true)
 
 
 try {
@@ -121,77 +158,79 @@ catch {
     exit
 }
 
-Write-Output "Getting the GPOs linked to the root domain $DNSDomain"
-#Get Root Domain linked GPOs and process them accordingly
-try {
-    $RootGPOs = Get-ADObject -Server $DNSDomain -Identity (Get-ADDomain -Identity $DNSDomain).distinguishedName -Properties name, distinguishedName, gPLink, gPOptions
-    
-}
-catch {
-    Write-Error "Error getting the GPOs linked to the root domain $DNSDomain $_"
-    Read-Host "Press any key to exit"
-    exit
-}#Loop through each root GPO
-Write-Output "Creating the GPO shapes and connecting them to the root domain"
-ForEach ($gpolink in $RootGPOs.gPlink -split "\]\[") {
-    #Add to our counters (for naming)
-    $gpoCount += 1 
-    $conCount += 1 
-    #get only the GUID of the gpo
-    $gpoGUID = ([Regex]::Match($gpoLink, '{[a-zA-Z0-9]{8}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{12}}')).Value 
-    #pull details for the GPO based on the GUID
+if ($IncludeGPOs) {
+    Write-Output "Getting the GPOs linked to the root domain $DNSDomain"
+    #Get Root Domain linked GPOs and process them accordingly
     try {
-        $gpo = Get-GPO -Guid $gpoGUID -Domain $DNSDomain
+        $RootGPOs = Get-ADObject -Server $DNSDomain -Identity (Get-ADDomain -Identity $DNSDomain).distinguishedName -Properties name, distinguishedName, gPLink, gPOptions
+        
     }
     catch {
-        Write-Warning "Error getting the GPO with GUID $gpoGUID $_"
-        Write-Warning "Skipping this GPO"
-        Continue
+        Write-Error "Error getting the GPOs linked to the root domain $DNSDomain $_"
+        Read-Host "Press any key to exit"
+        exit
+    }#Loop through each root GPO
+    Write-Output "Creating the GPO shapes and connecting them to the root domain"
+    ForEach ($gpolink in $RootGPOs.gPlink -split "\]\[") {
+        #Add to our counters (for naming)
+        $gpoCount += 1 
+        $conCount += 1 
+        #get only the GUID of the gpo
+        $gpoGUID = ([Regex]::Match($gpoLink, '{[a-zA-Z0-9]{8}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{4}[-][a-zA-Z0-9]{12}}')).Value 
+        #pull details for the GPO based on the GUID
+        try {
+            $gpo = Get-GPO -Guid $gpoGUID -Domain $DNSDomain
+        }
+        catch {
+            Write-Warning "Error getting the GPO with GUID $gpoGUID $_"
+            Write-Warning "Skipping this GPO"
+            Continue
+        }
+        #declare what we'll call the gpo shape 
+        $shapename = "g" + $gpoCount 
+        #Create the GPO shape
+        $shapeGPO = New-VisioShape -Master $MasterGPO -Position $Point_1_1 
+        #Set the shape properties
+        $ShapeGPO.Text = $GPO.DisplayName
+        $ShapeGPO.Name = $shapename
+        #Set the shape's custom properties
+        $GUID = "{" + $gpo.id.guid + "}"
+        If ($GPO.DisplayName) {
+            Set-VisioCustomProperty -Shape $ShapeGPO -Name "GPOName" -Value $GPO.DisplayName
+        }
+        If ($GPO.Description) {
+            Set-VisioCustomProperty -Shape $ShapeGPO -Name "Description" -Value $GPO.Description
+        }
+        If ($GPO.ID.Guid) {
+            Set-VisioCustomProperty -Shape $shapeGPO -Name "GUID" -Value $GUID
+        }
+        If ($GPO.GPOStatus) {
+            Set-VisioCustomProperty -Shape $shapeGPO -Name "Status" -Value $GPO.GpoStatus.ToString()
+        }
+        If ($GPO.CreationTime) {
+            Set-VisioCustomProperty -Shape $shapeGPO -Name "CreationTime" -Value $GPO.CreationTime.ToString()
+        }
+        If ($GPO.ModificationTime) {
+            Set-VisioCustomProperty -Shape $shapeGPO -Name "ModifiedTime" -Value $GPO.ModificationTime.ToString()
+        }
+        If ($GPO.WmiFilter) {
+            Set-VisioCustomProperty -Shape $shapeGPO -Name "WMIFilterName" -Value $GPO.WMIFilter.Name
+        }
+        #Create the shape's connections
+        $con = Connect-VisioShape -From $n0 -To $shapeGPO -Master $connector 
+        #Set the connections custom properties
+        $con.text = "GPO"
+        $con.name = "gcon" + $conCount #We name it like this so that later we can identify all GPO connections for formatting of the connector's text
+        $con_cells = New-VisioShapeCells
+        $con_cells.LineColor = "rgb(0,175,240)"
+        $con_cells.LineEndArrowSize = "3"
+        $con_cells.LineBeginArrowSize = "2"
+        $con_cells.LineEndArrow = "42"
+        $con_cells.LineBeginArrow = "4"
+        $con_cells.CharColor = "rgb(0,175,240)"
+        #Set the shape properties
+        Set-VisioShapeCells -Cells $con_cells -Shape $con     
     }
-    #declare what we'll call the gpo shape 
-    $shapename = "g" + $gpoCount 
-    #Create the GPO shape
-    $shapeGPO = New-VisioShape -Master $MasterGPO -Position $Point_1_1 
-    #Set the shape properties
-    $ShapeGPO.Text = $GPO.DisplayName
-    $ShapeGPO.Name = $shapename
-    #Set the shape's custom properties
-    $GUID = "{" + $gpo.id.guid + "}"
-    If ($GPO.DisplayName) {
-        Set-VisioCustomProperty -Shape $ShapeGPO -Name "GPOName" -Value $GPO.DisplayName
-    }
-    If ($GPO.Description) {
-        Set-VisioCustomProperty -Shape $ShapeGPO -Name "Description" -Value $GPO.Description
-    }
-    If ($GPO.ID.Guid) {
-        Set-VisioCustomProperty -Shape $shapeGPO -Name "GUID" -Value $GUID
-    }
-    If ($GPO.GPOStatus) {
-        Set-VisioCustomProperty -Shape $shapeGPO -Name "Status" -Value $GPO.GpoStatus.ToString()
-    }
-    If ($GPO.CreationTime) {
-        Set-VisioCustomProperty -Shape $shapeGPO -Name "CreationTime" -Value $GPO.CreationTime.ToString()
-    }
-    If ($GPO.ModificationTime) {
-        Set-VisioCustomProperty -Shape $shapeGPO -Name "ModifiedTime" -Value $GPO.ModificationTime.ToString()
-    }
-    If ($GPO.WmiFilter) {
-        Set-VisioCustomProperty -Shape $shapeGPO -Name "WMIFilterName" -Value $GPO.WMIFilter.Name
-    }
-    #Create the shape's connections
-    $con = Connect-VisioShape -From $n0 -To $shapeGPO -Master $connector 
-    #Set the connections custom properties
-    $con.text = "GPO"
-    $con.name = "gcon" + $conCount #We name it like this so that later we can identify all GPO connections for formatting of the connector's text
-    $con_cells = New-VisioShapeCells
-    $con_cells.LineColor = "rgb(0,175,240)"
-    $con_cells.LineEndArrowSize = "3"
-    $con_cells.LineBeginArrowSize = "2"
-    $con_cells.LineEndArrow = "42"
-    $con_cells.LineBeginArrow = "4"
-    $con_cells.CharColor = "rgb(0,175,240)"
-    #Set the shape properties
-    Set-VisioShapeCells -Cells $con_cells -Shape $con     
 }
 
 
@@ -222,7 +261,9 @@ ForEach ($ou in $OUs) {
         #Set custom properties of the shape
         Set-VisioCustomProperty -Shape $shape -Name "OU_Name" -Value $OU.Name
         Set-VisioCustomProperty -Shape $shape -Name "DistinguishedName" -Value $OU.DistinguishedName
-        Set-VisioCustomProperty -Shape $shape -Name "Linked_GPOs" -Value $OU.LinkedGroupPolicyObjects.Count
+        if ($IncludeGPOs) {
+            Set-VisioCustomProperty -Shape $shape -Name "Linked_GPOs" -Value $OU.LinkedGroupPolicyObjects.Count
+        }
                 
         #Connect the shape to the root domain shape
         Connect-VisioShape -From $n0 -To $shape -Master $connector | Out-Null
@@ -247,14 +288,16 @@ ForEach ($ou in $OUs) {
         #Set custom properties of the shape
         Set-VisioCustomProperty -Shape $shape -Name "OU_Name" -Value $OU.Name
         Set-VisioCustomProperty -Shape $shape -Name "DistinguishedName" -Value $OU.DistinguishedName
-        Set-VisioCustomProperty -Shape $shape -Name "Linked_GPOs" -Value $OU.LinkedGroupPolicyObjects.Count
+        if ($IncludeGPOs) {
+            Set-VisioCustomProperty -Shape $shape -Name "Linked_GPOs" -Value $OU.LinkedGroupPolicyObjects.Count
+        }
 
         #Connect the shape to the previous shape
         Connect-VisioShape -From $prevOUshape -To $shape -Master $connector | Out-Null
     }
 
     #If the OU had linked GPOs..
-    If ($OU.LinkedGroupPolicyObjects) {
+    If ($OU.LinkedGroupPolicyObjects -and $IncludeGPOs) {
         #Loop through each GPO
         Foreach ($gpoLink in $OU.LinkedGroupPolicyObjects) {
             #increase our counters
@@ -327,10 +370,11 @@ try {
     #set object properties (this is how we format the page)
     $ls.AvenueSizeX = 1
     $ls.AvenueSizeY = 1
-    $ls.LayoutDirection = "ToptoBottom"
+    $ls.LayoutDirection = $LayoutDirection
     $ls.ConnectorStyle = "Simple"
     $ls.ConnectorAppearance = "Straight"
     $ls.horizontalAlignment = "Left"
+    $ls.verticalAlignment = "Top"
     
     #Apply the layout object to the page
     Format-VisioPage -LayoutStyle $ls 
@@ -358,3 +402,7 @@ catch {
     Write-Warning "Error formatting the Visio page $_"
     Write-Output "Unless there were errors, the Visio document should be created, but may not be formatted correctly"
 }
+
+# Powershell garbage collection
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
